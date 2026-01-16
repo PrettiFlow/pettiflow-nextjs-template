8ac3b4b49c398fc94e78db2f0e2c11a950b841e1
Amit <amitdey1350@gmail.com>
2026-01-16 15:17:45 +0530

    idk

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-[#0a0a0a]">
-<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center gap-14 py-32 px-16 bg-white dark:bg-[#0a0a0a] sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-[#0a0a0a] font-sans dark:bg-[#0a0a0a]">
+<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center gap-14 py-32 px-16 bg-[#0a0a0a] dark:bg-[#0a0a0a] sm:items-start">
         <Image
           src="/prettiflow.svg"
           alt="PrettiFlow logo"
@@ -12,10 +12,10 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-zinc-50 dark:text-zinc-50">
             To get started, Prompt in the chat panel :)
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="max-w-md text-lg leading-8 text-zinc-400 dark:text-zinc-400">
 Prettiflow Makes software development easier and scalable         
           </p>
         </div>
